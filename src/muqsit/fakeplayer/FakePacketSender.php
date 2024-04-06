cdc5c31bbcd756647965642919c7ac1c2f61d896
Superomarking <71559522+Superomarking@users.noreply.github.com>
2024-04-06 04:55:23 +0300

    Updated to PM 5.14 (#43)

--- src/muqsit/fakeplayer/FakePacketSender.php
+++ src/muqsit/fakeplayer/FakePacketSender.php
@@ -8,7 +8,7 @@ use pocketmine\network\mcpe\PacketSender;
 
 final class FakePacketSender implements PacketSender{
 
-	public function send(string $payload, bool $immediate) : void{
+	public function send(string $payload, bool $immediate, ?int $receiptId) : void{
 	}
 
 	public function close(string $reason = "unknown reason") : void{
